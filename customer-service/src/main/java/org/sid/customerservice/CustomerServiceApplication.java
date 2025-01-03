a9b88dc373cb89575c7c74db32d74551fb037128
oussamamandour65 <oussamamandour65@gmail.com>
2025-01-03 22:58:33 +0100

    V3

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,16 +1,19 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.config.CustomerConfigParams;
 import org.sid.customerservice.entities.Customer;
 import org.sid.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
